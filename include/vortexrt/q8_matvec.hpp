2a6de65356b003068b67df3183cf3f861e9e4fe6
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-25 03:50:10 -0700

    q8: reserve cached pair and triplet descriptor state

--- include/vortexrt/q8_matvec.hpp
+++ include/vortexrt/q8_matvec.hpp
@@ -102,6 +102,10 @@ private:
     VkPipeline pipeline_ = VK_NULL_HANDLE;
     VkDescriptorPool descriptor_pool_ = VK_NULL_HANDLE;
     VkDescriptorSet descriptor_set_ = VK_NULL_HANDLE;
+    std::array<VkDescriptorSet, 2> pair_sets_{
+        VK_NULL_HANDLE,
+        VK_NULL_HANDLE,
+    };
     std::array<VkDescriptorSet, 3> triplet_sets_{
         VK_NULL_HANDLE,
         VK_NULL_HANDLE,
@@ -118,6 +122,18 @@ private:
     VkDeviceSize bound_weights_size_ = 0;
     VkDeviceSize bound_input_size_ = 0;
     VkDeviceSize bound_output_size_ = 0;
+
+    VkBuffer pair_bound_weights_ = VK_NULL_HANDLE;
+    VkBuffer pair_bound_input_ = VK_NULL_HANDLE;
+    VkBuffer pair_bound_output_ = VK_NULL_HANDLE;
+    std::array<std::uint32_t, 2> pair_bound_output_dims_{};
+    bool pair_descriptors_valid_ = false;
+
+    VkBuffer triplet_bound_weights_ = VK_NULL_HANDLE;
+    VkBuffer triplet_bound_input_ = VK_NULL_HANDLE;
+    VkBuffer triplet_bound_output_ = VK_NULL_HANDLE;
+    std::array<std::uint32_t, 3> triplet_bound_output_dims_{};
+    bool triplet_descriptors_valid_ = false;
 
     std::unordered_map<
         DispatchKey,
