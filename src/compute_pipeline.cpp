3b5c13a935d5716fad25dd6ddecde32d49641271
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-25 02:39:28 -0700

    runtime: reuse descriptors and recorded compute commands safely

--- src/compute_pipeline.cpp
+++ src/compute_pipeline.cpp
@@ -52,13 +52,15 @@ ComputePipeline::ComputePipeline(
 
     const auto spirv = load_spirv(spirv_path);
 
-    VkShaderModuleCreateInfo smci{VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO};
+    VkShaderModuleCreateInfo smci{};
+    smci.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
     smci.codeSize = spirv.size() * sizeof(std::uint32_t);
     smci.pCode = spirv.data();
 
     VkShaderModule shader = VK_NULL_HANDLE;
-    vk_check(vkCreateShaderModule(context_.device(), &smci, nullptr, &shader),
-             "vkCreateShaderModule failed");
+    vk_check(
+        vkCreateShaderModule(context_.device(), &smci, nullptr, &shader),
+        "vkCreateShaderModule failed");
 
     try {
         std::vector<VkDescriptorSetLayoutBinding> bindings(binding_count_);
@@ -69,93 +71,146 @@ ComputePipeline::ComputePipeline(
             bindings[i].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
         }
 
-        VkDescriptorSetLayoutCreateInfo dlci{
-            VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO
-        };
+        VkDescriptorSetLayoutCreateInfo dlci{};
+        dlci.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
         dlci.bindingCount = binding_count_;
         dlci.pBindings = bindings.data();
 
-        vk_check(vkCreateDescriptorSetLayout(
-                     context_.device(), &dlci, nullptr, &descriptor_set_layout_),
-                 "vkCreateDescriptorSetLayout failed");
+        vk_check(
+            vkCreateDescriptorSetLayout(
+                context_.device(),
+                &dlci,
+                nullptr,
+                &descriptor_set_layout_),
+            "vkCreateDescriptorSetLayout failed");
 
         VkPushConstantRange push{};
         push.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
         push.offset = 0;
         push.size = sizeof(std::uint32_t);
 
-        VkPipelineLayoutCreateInfo plci{VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO};
+        VkPipelineLayoutCreateInfo plci{};
+        plci.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         plci.setLayoutCount = 1;
         plci.pSetLayouts = &descriptor_set_layout_;
         plci.pushConstantRangeCount = 1;
         plci.pPushConstantRanges = &push;
 
-        vk_check(vkCreatePipelineLayout(context_.device(), &plci, nullptr, &pipeline_layout_),
-                 "vkCreatePipelineLayout failed");
+        vk_check(
+            vkCreatePipelineLayout(
+                context_.device(),
+                &plci,
+                nullptr,
+                &pipeline_layout_),
+            "vkCreatePipelineLayout failed");
 
-        VkPipelineShaderStageCreateInfo stage{
-            VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO
-        };
+        VkPipelineShaderStageCreateInfo stage{};
+        stage.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
         stage.stage = VK_SHADER_STAGE_COMPUTE_BIT;
         stage.module = shader;
         stage.pName = "main";
 
-        VkComputePipelineCreateInfo cpci{VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO};
+        VkComputePipelineCreateInfo cpci{};
+        cpci.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
         cpci.stage = stage;
         cpci.layout = pipeline_layout_;
 
-        vk_check(vkCreateComputePipelines(
-                     context_.device(), VK_NULL_HANDLE, 1, &cpci, nullptr, &pipeline_),
-                 "vkCreateComputePipelines failed");
+        vk_check(
+            vkCreateComputePipelines(
+                context_.device(),
+                VK_NULL_HANDLE,
+                1,
+                &cpci,
+                nullptr,
+                &pipeline_),
+            "vkCreateComputePipelines failed");
 
         VkDescriptorPoolSize pool_size{};
         pool_size.type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
         pool_size.descriptorCount = binding_count_;
 
-        VkDescriptorPoolCreateInfo dpci{VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO};
+        VkDescriptorPoolCreateInfo dpci{};
+        dpci.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
         dpci.maxSets = 1;
         dpci.poolSizeCount = 1;
         dpci.pPoolSizes = &pool_size;
 
-        vk_check(vkCreateDescriptorPool(context_.device(), &dpci, nullptr, &descriptor_pool_),
-                 "vkCreateDescriptorPool failed");
+        vk_check(
+            vkCreateDescriptorPool(
+                context_.device(),
+                &dpci,
+                nullptr,
+                &descriptor_pool_),
+            "vkCreateDescriptorPool failed");
 
-        VkDescriptorSetAllocateInfo dsai{VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO};
+        VkDescriptorSetAllocateInfo dsai{};
+        dsai.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
         dsai.descriptorPool = descriptor_pool_;
         dsai.descriptorSetCount = 1;
         dsai.pSetLayouts = &descriptor_set_layout_;
 
-        vk_check(vkAllocateDescriptorSets(context_.device(), &dsai, &descriptor_set_),
-                 "vkAllocateDescriptorSets failed");
+        vk_check(
+            vkAllocateDescriptorSets(
+                context_.device(),
+                &dsai,
+                &descriptor_set_),
+            "vkAllocateDescriptorSets failed");
 
-        VkCommandPoolCreateInfo pool_ci{VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO};
+        VkCommandPoolCreateInfo pool_ci{};
+        pool_ci.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
         pool_ci.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
         pool_ci.queueFamilyIndex = context_.compute_queue_family();
 
-        vk_check(vkCreateCommandPool(context_.device(), &pool_ci, nullptr, &command_pool_),
-                 "vkCreateCommandPool failed");
+        vk_check(
+            vkCreateCommandPool(
+                context_.device(),
+                &pool_ci,
+                nullptr,
+                &command_pool_),
+            "vkCreateCommandPool failed");
 
-        VkCommandBufferAllocateInfo cbai{VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO};
+        VkCommandBufferAllocateInfo cbai{};
+        cbai.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
         cbai.commandPool = command_pool_;
         cbai.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
         cbai.commandBufferCount = 1;
 
-        vk_check(vkAllocateCommandBuffers(context_.device(), &cbai, &command_buffer_),
-                 "vkAllocateCommandBuffers failed");
+        vk_check(
+            vkAllocateCommandBuffers(
+                context_.device(),
+                &cbai,
+                &command_buffer_),
+            "vkAllocateCommandBuffers failed");
 
-        VkFenceCreateInfo fci{VK_STRUCTURE_TYPE_FENCE_CREATE_INFO};
-        vk_check(vkCreateFence(context_.device(), &fci, nullptr, &fence_),
-                 "vkCreateFence failed");
+        VkFenceCreateInfo fci{};
+        fci.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+        vk_check(
+            vkCreateFence(
+                context_.device(),
+                &fci,
+                nullptr,
+                &fence_),
+            "vkCreateFence failed");
 
         if (context_.capabilities().timestamp_valid_bits != 0) {
-            VkQueryPoolCreateInfo qpci{VK_STRUCTURE_TYPE_QUERY_POOL_CREATE_INFO};
+            VkQueryPoolCreateInfo qpci{};
+            qpci.sType = VK_STRUCTURE_TYPE_QUERY_POOL_CREATE_INFO;
             qpci.queryType = VK_QUERY_TYPE_TIMESTAMP;
             qpci.queryCount = 2;
-            vk_check(vkCreateQueryPool(context_.device(), &qpci, nullptr, &query_pool_),
-                     "vkCreateQueryPool failed");
+            vk_check(
+                vkCreateQueryPool(
+                    context_.device(),
+                    &qpci,
+                    nullptr,
+                    &query_pool_),
+                "vkCreateQueryPool failed");
         }
+
+        bound_buffers_.assign(binding_count_, VK_NULL_HANDLE);
+        bound_sizes_.assign(binding_count_, 0);
     } catch (...) {
         vkDestroyShaderModule(context_.device(), shader, nullptr);
+        cleanup();
         throw;
     }
 
@@ -163,39 +218,62 @@ ComputePipeline::ComputePipeline(
 }
 
 ComputePipeline::~ComputePipeline() {
+    cleanup();
+}
+
+void ComputePipeline::cleanup() noexcept {
     if (query_pool_ != VK_NULL_HANDLE) {
         vkDestroyQueryPool(context_.device(), query_pool_, nullptr);
+        query_pool_ = VK_NULL_HANDLE;
     }
     if (fence_ != VK_NULL_HANDLE) {
         vkDestroyFence(context_.device(), fence_, nullptr);
+        fence_ = VK_NULL_HANDLE;
     }
     if (command_pool_ != VK_NULL_HANDLE) {
         vkDestroyCommandPool(context_.device(), command_pool_, nullptr);
+        command_pool_ = VK_NULL_HANDLE;
+        command_buffer_ = VK_NULL_HANDLE;
     }
     if (descriptor_pool_ != VK_NULL_HANDLE) {
         vkDestroyDescriptorPool(context_.device(), descriptor_pool_, nullptr);
+        descriptor_pool_ = VK_NULL_HANDLE;
+        descriptor_set_ = VK_NULL_HANDLE;
     }
     if (pipeline_ != VK_NULL_HANDLE) {
         vkDestroyPipeline(context_.device(), pipeline_, nullptr);
+        pipeline_ = VK_NULL_HANDLE;
     }
     if (pipeline_layout_ != VK_NULL_HANDLE) {
         vkDestroyPipelineLayout(context_.device(), pipeline_layout_, nullptr);
+        pipeline_layout_ = VK_NULL_HANDLE;
     }
     if (descriptor_set_layout_ != VK_NULL_HANDLE) {
-        vkDestroyDescriptorSetLayout(context_.device(), descriptor_set_layout_, nullptr);
+        vkDestroyDescriptorSetLayout(
+            context_.device(),
+            descriptor_set_layout_,
+            nullptr);
+        descriptor_set_layout_ = VK_NULL_HANDLE;
     }
 }
 
-DispatchStats ComputePipeline::run(
-    const std::vector<Buffer*>& buffers,
-    std::uint32_t element_count,
-    std::uint32_t repetitions) {
+void ComputePipeline::update_descriptors(
+    const std::vector<Buffer*>& buffers) {
 
-    if (buffers.size() != binding_count_) {
-        throw std::runtime_error("ComputePipeline::run buffer count mismatch");
+    bool changed = false;
+    for (std::uint32_t i = 0; i < binding_count_; ++i) {
+        if (buffers[i] == nullptr) {
+            throw std::runtime_error("ComputePipeline::run received null buffer");
+        }
+        if (bound_buffers_[i] != buffers[i]->handle() ||
+            bound_sizes_[i] != buffers[i]->size()) {
+            changed = true;
+            break;
+        }
     }
-    if (repetitions == 0) {
-        throw std::runtime_error("ComputePipeline::run repetitions must be non-zero");
+
+    if (!changed) {
+        return;
     }
 
     std::vector<VkDescriptorBufferInfo> infos(binding_count_);
@@ -206,12 +284,16 @@ DispatchStats ComputePipeline::run(
         infos[i].offset = 0;
         infos[i].range = buffers[i]->size();
 
-        writes[i] = {VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET};
+        writes[i] = {};
+        writes[i].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
         writes[i].dstSet = descriptor_set_;
         writes[i].dstBinding = i;
         writes[i].descriptorCount = 1;
         writes[i].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
         writes[i].pBufferInfo = &infos[i];
+
+        bound_buffers_[i] = buffers[i]->handle();
+        bound_sizes_[i] = buffers[i]->size();
     }
 
     vkUpdateDescriptorSets(
@@ -220,13 +302,28 @@ DispatchStats ComputePipeline::run(
         writes.data(),
         0,
         nullptr);
+}
 
-    vk_check(vkResetFences(context_.device(), 1, &fence_), "vkResetFences failed");
-    vk_check(vkResetCommandBuffer(command_buffer_, 0), "vkResetCommandBuffer failed");
+void ComputePipeline::record_commands(
+    std::uint32_t element_count,
+    std::uint32_t repetitions) {
 
-    VkCommandBufferBeginInfo begin{VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO};
-    begin.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-    vk_check(vkBeginCommandBuffer(command_buffer_, &begin), "vkBeginCommandBuffer failed");
+    if (command_recording_valid_ &&
+        recorded_element_count_ == element_count &&
+        recorded_repetitions_ == repetitions) {
+        return;
+    }
+
+    vk_check(
+        vkResetCommandBuffer(command_buffer_, 0),
+        "vkResetCommandBuffer failed");
+
+    VkCommandBufferBeginInfo begin{};
+    begin.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    begin.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+    vk_check(
+        vkBeginCommandBuffer(command_buffer_, &begin),
+        "vkBeginCommandBuffer failed");
 
     if (query_pool_ != VK_NULL_HANDLE) {
         vkCmdResetQueryPool(command_buffer_, query_pool_, 0, 2);
@@ -237,7 +334,10 @@ DispatchStats ComputePipeline::run(
             0);
     }
 
-    vkCmdBindPipeline(command_buffer_, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline_);
+    vkCmdBindPipeline(
+        command_buffer_,
+        VK_PIPELINE_BIND_POINT_COMPUTE,
+        pipeline_);
     vkCmdBindDescriptorSets(
         command_buffer_,
         VK_PIPELINE_BIND_POINT_COMPUTE,
@@ -271,41 +371,84 @@ DispatchStats ComputePipeline::run(
             1);
     }
 
-    vk_check(vkEndCommandBuffer(command_buffer_), "vkEndCommandBuffer failed");
+    vk_check(
+        vkEndCommandBuffer(command_buffer_),
+        "vkEndCommandBuffer failed");
 
-    VkSubmitInfo submit{VK_STRUCTURE_TYPE_SUBMIT_INFO};
+    recorded_element_count_ = element_count;
+    recorded_repetitions_ = repetitions;
+    command_recording_valid_ = true;
+}
+
+DispatchStats ComputePipeline::run(
+    const std::vector<Buffer*>& buffers,
+    std::uint32_t element_count,
+    std::uint32_t repetitions) {
+
+    if (buffers.size() != binding_count_) {
+        throw std::runtime_error("ComputePipeline::run buffer count mismatch");
+    }
+    if (element_count == 0) {
+        throw std::runtime_error("ComputePipeline::run element count must be non-zero");
+    }
+    if (repetitions == 0) {
+        throw std::runtime_error("ComputePipeline::run repetitions must be non-zero");
+    }
+
+    update_descriptors(buffers);
+    record_commands(element_count, repetitions);
+
+    vk_check(
+        vkResetFences(context_.device(), 1, &fence_),
+        "vkResetFences failed");
+
+    VkSubmitInfo submit{};
+    submit.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
     submit.commandBufferCount = 1;
     submit.pCommandBuffers = &command_buffer_;
 
     const auto cpu_start = std::chrono::steady_clock::now();
 
-    vk_check(vkQueueSubmit(context_.compute_queue(), 1, &submit, fence_),
-             "vkQueueSubmit failed");
-    vk_check(vkWaitForFences(
-                 context_.device(), 1, &fence_, VK_TRUE, UINT64_MAX),
-             "vkWaitForFences failed");
+    vk_check(
+        vkQueueSubmit(
+            context_.compute_queue(),
+            1,
+            &submit,
+            fence_),
+        "vkQueueSubmit failed");
+    vk_check(
+        vkWaitForFences(
+            context_.device(),
+            1,
+            &fence_,
+            VK_TRUE,
+            UINT64_MAX),
+        "vkWaitForFences failed");
 
     const auto cpu_end = std::chrono::steady_clock::now();
 
     DispatchStats stats{};
     stats.cpu_ms =
-        std::chrono::duration<double, std::milli>(cpu_end - cpu_start).count();
+        std::chrono::duration<double, std::milli>(
+            cpu_end - cpu_start).count();
 
     if (query_pool_ != VK_NULL_HANDLE) {
         std::uint64_t ts[2]{};
-        vk_check(vkGetQueryPoolResults(
-                     context_.device(),
-                     query_pool_,
-                     0,
-                     2,
-                     sizeof(ts),
-                     ts,
-                     sizeof(std::uint64_t),
-                     VK_QUERY_RESULT_64_BIT | VK_QUERY_RESULT_WAIT_BIT),
-                 "vkGetQueryPoolResults failed");
+        vk_check(
+            vkGetQueryPoolResults(
+                context_.device(),
+                query_pool_,
+                0,
+                2,
+                sizeof(ts),
+                ts,
+                sizeof(std::uint64_t),
+                VK_QUERY_RESULT_64_BIT | VK_QUERY_RESULT_WAIT_BIT),
+            "vkGetQueryPoolResults failed");
 
         std::uint64_t delta = ts[1] - ts[0];
-        const std::uint32_t bits = context_.capabilities().timestamp_valid_bits;
+        const std::uint32_t bits =
+            context_.capabilities().timestamp_valid_bits;
 
         if (bits > 0 && bits < 64) {
             const std::uint64_t mask = (1ull << bits) - 1ull;
@@ -314,7 +457,8 @@ DispatchStats ComputePipeline::run(
 
         stats.gpu_ms =
             (static_cast<double>(delta) *
-             static_cast<double>(context_.capabilities().timestamp_period_ns)) /
+             static_cast<double>(
+                 context_.capabilities().timestamp_period_ns)) /
             1'000'000.0;
         stats.gpu_timing_available = true;
     }
